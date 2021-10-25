ac969bfecaca1c8c1502d019a88c01651ae0c1b6
dMosca <devinmosca@yahoo.com>
2021-10-25 14:39:28 -0600

    Updated api routes, Seeds Still not working.

--- routes/api.js
+++ routes/api.js
@@ -1,48 +1,44 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
-router.get("/api/workout", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.get("/api/workouts", async (req, res) => {
+  const workouts = await Workout.find();
+  try {
+    res.json(workouts);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.get("/api/workout/range", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.get("/api/workouts/range", async (req, res) => {
+  const workouts = await Workout.find();
+  try {
+    res.json(workouts);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.post("/api/workout", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
+router.post("/api/workouts", async ({ body }, res) => {
+  const newWorkout = Workout.create(body)
+    try {
+      res.json(newWorkout);
+    } catch (err) {
       res.status(400).json(err);
-    });
+    };
 });
 
-// router.put("/api/workout/:id", ({ body }, res) => {
-//   Workout.insertMany(body)
-//     .then(dbWorkout => {
-//       res.json(dbWorkout);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
-
+router.put("/api/workouts/:id", async (req, res) => {
+  const updateWorkout = await Workout.findById(req.params.id, {
+    $push: {
+      exercises: req.body,
+    },
+  });
+  try {
+    res.status(200).json(updateWorkout);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 module.exports = router;
