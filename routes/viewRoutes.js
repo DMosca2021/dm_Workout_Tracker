3f6f564d3759ce67b18b68a947b7e69f89af5049
dMosca <devinmosca@yahoo.com>
2021-10-26 16:26:58 -0600

    Did not need root route in viewRoutes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
 const path = require('path');
 
-// dont need root route 
-// router.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'));
-// });
-
 router.get('/exercise', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
